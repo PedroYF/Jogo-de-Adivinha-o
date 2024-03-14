f783129b6f1faaecefcd16819fb7d19df73e9726
PedroYF <161763343+PedroYF@users.noreply.github.com>
2024-03-14 20:07:20 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,26 +1,28 @@
-from random import randint
+from random import randint #importando randomizer
 
-while True:
-    numx = randint(1, 100)
+while True: #iniciando o jogo
+    numx = randint(1, 100) #pegando número x
 
     numr = int(input('Qual número o programa pensou? de 1 a 100: '))
     tentativas = 1
-    while numr != numx:
-        if numx > numr:
+    chute = [numr] #abrindo uma lista de chutes
+    while numr != numx: #Começando a repetição do jogo
+        if numx > numr: #caso número x seja maior que chute
             print('-'*10)
             print('O número que o programa pensou é maior que {}'.format(numr))
             numr = int(input('Digite um outro número '))
             tentativas = tentativas + 1
-        else:
+            chute.append(numr) #adiciona chute à lista
+        else: #Caso número x seja menor que chute
             print('-'*10)
             print('O número que o programa pensou é menor que {}!'.format(numr))
             numr = int(input('Digite um outro número '))
             tentativas = tentativas + 1
-
+            chute.append(numr) #adiciona chute à lista
     print('-'*10)
     print('Parabéns, você acertou em {} tentativas, o número que o programa pensou era {}!'.format(tentativas, numx))
-    
-    continuar = input('Deseja jogar outra partida?? (s/n) ')
+    print('Os seus chutes foram: {}'.format(chute))
+    continuar = input('Deseja jogar outra partida?? (s/n) ') #valindando caso queira loopar o programa
     pergunta = ['sim', 's', 'yes']
     if continuar not in pergunta:
         break
